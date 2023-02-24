15e67ec275aeb7f261e649374671536037af069b
Asha Luckins <ashaluckinsanoop@gmail.com>
2023-02-24 13:11:28 -0800

    revert to the previous way of defining the route using createBrouserRouter and RouterProvider

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { createRoutesFromElements, Route } from "react-router-dom";
 
 import HomePage from "./pages/Home";
 import ProductPage from "./pages/Products";
 
-// const router = createBrowserRouter([
-//   { path: "/", element: <HomePage /> },
-//   { path: "/products", element: <ProductPage /> },
-// ]);
-
-const routeDefinitions = createRoutesFromElements(
-  <Route>
-    <Route path="/" element={<HomePage />} />
-    <Route path="/products" element={<ProductPage />} />
-  </Route>
-);
-
-const router = createBrowserRouter(routeDefinitions);
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/products", element: <ProductPage /> },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
