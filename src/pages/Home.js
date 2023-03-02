4d93d2b7d2f0702c607463e157cd5d958cd15e03
Asha Luckins <ashaluckinsanoop@gmail.com>
2023-03-02 10:33:56 -0800

    add navigate button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 function HomePage() {
+  const navigate = useNavigate();
+  const navigateHandler = () => {
+    navigate("/products");
+  };
+
   return (
     <>
       <h1>HomePage</h1>
       <p>
         Go to the <Link to="/products">Products Page</Link>
       </p>
+      <button onClick={navigateHandler}>Navigate</button>
     </>
   );
 }
