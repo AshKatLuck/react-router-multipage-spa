9f3a11e2b8c54017dd72b2481d423d32633efd34
Asha Luckins <ashaluckinsanoop@gmail.com>
2023-03-01 17:04:02 -0800

    add navbar and styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,12 @@
+import { Link } from "react-router-dom";
 function HomePage() {
-  return <h1>HomePage</h1>;
+  return (
+    <>
+      <h1>HomePage</h1>
+      <p>
+        Go to the <Link to="/products">Products Page</Link>
+      </p>
+    </>
+  );
 }
 export default HomePage;
